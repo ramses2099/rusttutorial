2b4adf4c1f2cbb4e24439524e2a0b1afb4b3f2ac
ramses2099 <ramses2099@gmail.com>
2023-07-20 09:13:09 -0400

    implement module in rust

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,10 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, Error, Write};
 
-//struct
-struct Customer {
-    name: String,
-    address: String,
-    balance: f32,
-}
+mod restaurant;
+use crate::restaurant::order_food;
 fn main() {
-    //instant
-    let mut bob = Customer {
-        name: String::from("bob Perez"),
-        address: String::from("23 los rios"),
-        balance: 5.00,
-    };
+  order_food();
+
 
 }

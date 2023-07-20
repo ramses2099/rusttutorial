5b3e40f78f6f605b5d37334bf4650e108e776afc
ramses2099 <ramses2099@gmail.com>
2023-07-20 08:33:23 -0400

    new changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#![allow(unused)]
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, Error, Write};
+
+//struct
+struct Customer {
+    name: String,
+    address: String,
+    balance: f32,
+}
 fn main() {
-    println!("Hello, world!");
+    //instant
+    let mut bob = Customer {
+        name: String::from("bob Perez"),
+        address: String::from("23 los rios"),
+        balance: 5.00,
+    };
+
 }
